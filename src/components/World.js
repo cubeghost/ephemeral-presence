97539baff9207666282b055a84494da4be21a1c7
Glitch (cubeghost-presence-experiment) <none>
2019-05-15 02:01:19 +0000

    😄🍥 Checkpoint ./webpack.config.js:261358/235 ./.glitch-assets:261358/802 ./watch.json:261358/18 ./src/components/World.js:261358/94

--- src/components/World.js
+++ src/components/World.js
@@ -2,12 +2,23 @@ import React from 'react';
 
 const styles = {
   zIndex: -1,
+  position: 'fixed',
   transform: 'translateX(50%) translateY(50%)',
 };
 
+const Sprite = ({ src, x, y }) => (
+  <img 
+    src={src}
+    style={{
+      position: 'fixed',
+      transform: `translateX(${x}) translateY(${y})`
+    }}
+  />
+);
+
 const World = () => (
   <div style={styles}>
-    hi
+    <img src="https://media.giphy.com/media/3otO6xRxnsZ8213SJa/100w.webp" />
   </div>
 );
 
