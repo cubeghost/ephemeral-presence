3bf957c6dece34ee26dc767eff861460ff8647ef
Glitch (cubeghost-presence-experiment) <none>
2019-05-15 01:36:18 +0000

    🌶👩‍👩‍👦 Checkpoint ./src/components/World.js:261358/393 ./src/client.js:261358/72

---
+++ src/components/World.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const styles = {
+  zIndex: -1,
+  transform: 'translateX(50%) translateY(50%)',
+};
+
+const World = () => (
+  <div style={styles}>
+    hi
+  </div>
+);
+
+export default World;
