b9daf9ef3380cdb1e13fc79c8e357549e4ad5807
Glitch (ephemeral-presence) <none>
2019-05-26 22:58:51 +0000

    🌜🏨 Checkpoint ./src/filter.js:261358/635

--- src/filter.js
+++ src/filter.js
@@ -42,7 +42,36 @@
 */
 
 const list = [
-  ''
+  'beaner',
+  'bitch',
+  'chink',
+  'cunt',
+  'dyke',
+  'fag',
+  'faggot',
+  'faggots',
+  'gook',
+  'jigaboo',
+  'kike',
+  'kikes',
+  'kyke',
+  'nigga',
+  'nigger',
+  'niggers',
+  'paki',
+  'pewdiepie',
+  'retard',
+  'retarded',
+  'sandnigger',
+  'slut',
+  'spic',
+  'spick',
+  'tard',
+  'towelhead',
+  'tranny',
+  'trannies',
+  'wetback',
+  'whore'
 ];
 
 export default list;
