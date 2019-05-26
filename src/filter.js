37fca20beb38ce11453e089693d90c4ca0ab8568
Glitch (ephemeral-presence) <none>
2019-05-26 22:47:51 +0000

    ⛩🏟 Checkpoint ./src/filter.js:261358/399

---
+++ src/filter.js
@@ -0,0 +1,48 @@
+/*
+  
+  WARNING:
+  THIS IS A BANNED WORDS FILTER 
+  SO THERE ARE RACIAL SLURS ON IT
+  DONT SCROLL DOWN UNLESS NECESSARY
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+*/
+
+const list = [
+  ''
+];
+
+export default list;
