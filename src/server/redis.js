f5f37819cb3c19485f7012e2fa330749c88dc71a
Glitch (ephemeral-presence) <none>
2025-01-24 15:41:37 +0000

    🏡🍥 Checkpoint ./src/server/redis.js:261358/2 ./package.json:261358/5574

--- src/server/redis.js
+++ src/server/redis.js
@@ -1,4 +1,4 @@
-const redis = require('redis');
+const redis = require('ioredis');
 const promisifyAll = require('util-promisifyall');
 const serialize = require('serialize-javascript');
 const debug = require('debug')('presence:redis');
